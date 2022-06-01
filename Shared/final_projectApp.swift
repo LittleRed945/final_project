008c51e0b1bcd51f22d9ad52309ab778420c5db0
erwin <erwinis001@gmail,com>
2022-06-01 11:19:05 +0800

    Initial Commit

---
+++ Shared/final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  final_projectApp.swift
+//  Shared
+//
+//  Created by  Erwin on 2022/6/1.
+//
+
+import SwiftUI
+
+@main
+struct final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
